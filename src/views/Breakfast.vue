5edf390c35505f8e7cac634370def18f98eb3f75
Bruce <brucegmaina@gmail.com>
2021-08-17 18:54:19 +0300

    Breakfast edit

--- src/views/Breakfast.vue
+++ src/views/Breakfast.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="breakfast-container">
+    <div id="back" @click="backtoFoods">
+      <img src="..//assets/imgs/back.svg" />
+    </div>
+    <div class="titleandimage">
       <span class="brekotitle">Karibu Chai</span>
+      <img class="breakfastimage" src="..//assets/imgs/tea.png">
+    </div>
   </div>
 </template>
 
@@ -18,30 +24,35 @@ export default {
 </script>
 
 <style scoped>
-.breakfast-container {
-  padding: 10px;
+.breakfast-container{
   background-color: #f1eeee;
   height: 650px;
   width: 290px;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  border-radius: 35px;
   overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  position: relative;
 }
-#back {
-  float: left;
+.brekotitle{
+  font-size: 170%;
+  font-weight: bold;
+  margin-top: 50%;
+  margin-left: 5%;
+  text-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+#back{
   cursor: pointer;
   height: 20px;
   width: 20px;
   padding: 20px 0px 0px 10px;
   border: none;
+  margin-bottom: 4%;
 }
-.btn{
-    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-    width:40%;
-    padding: 10px 15px;
-    border-radius: 8px;
-    margin-bottom: 2em;
-    cursor: pointer;
+.titleandimage{
+  display: flex;
+  border: 1px solid red;
+}
+.breakfastimage{
+ 
 }
 </style>
