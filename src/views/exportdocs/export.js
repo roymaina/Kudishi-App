deb00dd26442a7b1c25611445bb372cdf9291d1b
Bruce <brucegmaina@gmail.com>
2021-08-31 13:07:33 +0300

    Cart and finalizations

---
+++ src/views/exportdocs/export.js
@@ -0,0 +1,56 @@
+export const icons = [
+    {
+      icon: require("@/assets/imgs/Ngwaci.jpg"),
+      name: "Ngwaci",
+      price: "Ksh.20",
+      router: " ",
+    },
+    {
+      icon: require("@/assets/imgs/chapo.png"),
+      name: "Chapati",
+      price: "Ksh.10",
+      router: " ",
+    },
+    {
+      icon: require("@/assets/imgs/Chai.jpg"),
+      name: "Chai",
+      price: "Ksh.10",
+      router: " ",
+    },
+    {
+      icon: require("@/assets/imgs/Boiled-egg.jpg"),
+      name: "Boiled Eggs",
+      price: "Ksh.15",
+      router: " ",
+    },
+    {
+      icon: require("@/assets/imgs/mandazi.jpg"),
+      name: "Mandazi",
+      price: "Ksh.10",
+      router: " ",
+    },
+    {
+      icon: require("@/assets/imgs/mkate.jpg"),
+      name: "Loaf",
+      price: "Ksh.10",
+      router: " ",
+    },
+    {
+      icon: require("@/assets/imgs/samosa.jpg"),
+      name: "Samosa",
+      price: "Ksh.25",
+      router: " ",
+    },
+    {
+      icon: require("@/assets/imgs/smokie.jpg"),
+      name: "Smokie",
+      price: "Ksh.25",
+      router: " ",
+    },
+    {
+      icon: require("@/assets/imgs/kahawa.jpg"),
+      name: "Coffee",
+      price: "Ksh.10",
+      router: " ",
+    },
+  ]
